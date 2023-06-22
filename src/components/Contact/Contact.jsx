d1f0a343c0b9c7c84cc9b6bb0aa5b0c4311a07c9
Siar Wahidi <siar.wahidi@gmail.com>
2023-06-22 15:19:35 -0400

    Added some styling to the contact us form. Setup the handle change function to grab data entered by user when filling out the form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,43 +9,65 @@ const Contact = () => {
     emailBody: "",
   });
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <section className="m-5">
       <span className="text-xl">Get In Touch</span>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col mt-5">
           <input
-            className="p-3 bg-beige rounded-md mb-2"
+            className="p-3 bg-beige rounded-md mb-2 shadow-md shadow-slate-600"
             type="text"
             placeholder="First Name"
+            name="firstName"
             value={formData.firstName}
+            onChange={handleChange}
           />
           <input
-            className="p-3 bg-beige rounded-md mb-2"
+            className="p-3 bg-beige rounded-md mb-2 shadow-md shadow-slate-600"
             type="text"
             placeholder="Last Name"
+            name="lastName"
             value={formData.lastName}
+            onChange={handleChange}
           />
         </div>
         <div className="flex flex-col">
           <input
-            className="p-3 bg-beige rounded-md mb-2"
+            className="p-3 bg-beige rounded-md mb-2 shadow-md shadow-slate-600"
             type="email"
             placeholder="Email"
+            name="email"
             value={formData.email}
+            onChange={handleChange}
           />
           <input
-            className="p-3 bg-beige rounded-md mb-2"
+            className="p-3 bg-beige rounded-md mb-2 shadow-md shadow-slate-600"
             type="number"
             placeholder="Phone Number"
+            name="phoneNumber"
             value={formData.phoneNumber}
+            onChange={handleChange}
           />
         </div>
         <div>
           <textarea
-            className="w-full h-52 p-3 bg-beige rounded-md mb-2"
+            className="w-full h-52 p-3 bg-beige rounded-md mb-2 shadow-md shadow-slate-600"
             placeholder="Type your message here"
+            name="emailBody"
             value={formData.emailBody}
+            onChange={handleChange}
           ></textarea>
         </div>
         <div className="flex justify-center">
