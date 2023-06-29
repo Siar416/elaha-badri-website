c22a3e547fbd39cf58b33d849a0c7c6a9db640db
Siar Wahidi <siar.wahidi@gmail.com>
2023-06-29 14:12:56 -0400

    Added coming soon text to resources page

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -1,5 +1,9 @@
 const Resources = () => {
-  return <div>Resources</div>;
+  return (
+    <section className="h-screen flex justify-center items-center text-6xl">
+      <span>Coming soon!</span>
+    </section>
+  );
 };
 
 export default Resources;
